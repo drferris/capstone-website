90411707ca0363fdd7110baf6de237d54d5c0669
Dave Ferris <dave@entrision.com>
2014-01-29 15:16:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapstoneWebsite::Application.config.secret_token = '3040440c2b2c5d4ba728a93f43e0c3a7e1e77f4f24a2cd6c69865ad4fd0495faf9d278ab6101c81f61e9a24c7c4cf436976d6af032efe449967ec7225cfaa8af'
